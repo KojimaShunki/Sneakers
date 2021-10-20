4ca751f24b4891cece2c18d12444202d2814116a
KojimaShunki <90170219+KojimaShunki@users.noreply.github.com>
2021-10-21 04:42:50 +0900

    pickup commit

--- js/main.js
+++ js/main.js
@@ -32,3 +32,47 @@ $(function(){
   return false;
   });
 });
+
+
+var fadeConts = document.querySelectorAll('.fadeConts'); // フェードさせる要素の取得
+var fadeContsRect = []; // 要素の位置を取得するための配列
+var fadeContsTop = []; // 要素の位置を取得するための配列
+var windowY = window.pageYOffset; // ウィンドウのスクロール位置
+var windowH = window.innerHeight; // ウィンドウの高さ
+var remainder = 100; // ちょっとはみ出させる部分
+// 要素の位置を取得
+for (var i = 0; i < fadeConts.length; i++) {
+  fadeContsRect.push(fadeConts[i].getBoundingClientRect());
+}
+for (var i = 0; i < fadeContsRect.length; i++) {
+  fadeContsTop.push(fadeContsRect[i].top + windowY);
+}
+// ウィンドウがリサイズされたら、ウィンドウの高さを再取得
+window.addEventListener('resize', function () {
+  windowH = window.innerHeight;
+});
+// スクロールされたら
+window.addEventListener('scroll', function () {
+  // スクロール位置を取得
+  windowY = window.pageYOffset;
+  
+  for (var i = 0; i < fadeConts.length; i++) {
+    // 要素が画面の下端にかかったら
+    if(windowY > fadeContsTop[i] - windowH + remainder) {
+      // .showを付与
+      fadeConts[i].classList.add('show');
+    }
+  }
+});
+
+$(function() {
+  $('.slider').slick(); 
+});
+
+$('.slider').slick({
+  slidesToShow: 3,
+});
+
+$('.slider').slick({
+  nextArrow: '<button type="button" class="slick-next"></button>',
+});
